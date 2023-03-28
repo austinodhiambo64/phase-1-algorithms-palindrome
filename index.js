e6311685b3d45a67b8566a613b319e2f985c53b3
Austin <austinodhiambo64@gmail.com>
2023-03-28 13:28:18 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
+  arr0 = string.split('');
+  reversedLetters = arr0.reverse();
+  reversedString = reversedLetters.join('');
+
+  if (string ==reversedString){
+    return true
+  }
+  else {
+    return false
+  }
+
+
 }
 
 /* 
   Add your pseudocode here
+  arr split into letters
+  reversed
+  join()
+  if condition
+  return true
+  else 
 */
 
 /*
   Add written explanation of your solution here
+
+  the functions uses the split()method to seperate the substrings, then reverses them and tests to see whether it is equal to the initial string
 */
 
 // You can run `node index.js` to view these console logs
